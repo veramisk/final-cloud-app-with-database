a640e29bf6493625fdf98e4e883e5574ebf3464c
Veramisk <veramisk@gmail.com>
2023-01-24 05:03:41 -0500

    Another changes

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -48,7 +48,7 @@ class Learner(models.Model):
     social_link = models.URLField(max_length=200)
 
     def __str__(self):
-        return self.user.username + "," + \
+         return self.user.username + "," + \
                self.occupation
 
 
@@ -105,15 +105,29 @@ class Enrollment(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE,default="")
+    content = models.CharField(max_length=200,default="")
+    grade = models.IntegerField(default=50)
 
-    # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    def __str__(self):
+        return "Question: " + self.content
+
+# <HINT> A sample model method to calculate if learner get the score of the question
+#def is_get_score(self, selected_ids):
+#    all_answers = self.choice_set.filter(is_correct=True).count()
+#    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+#    if all_answers == selected_correct:
+#        return True
+#    else:
+#        return False
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 
 #  <HINT> Create a Choice Model with:
@@ -123,7 +137,13 @@ class Enrollment(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 # class Choice(models.Model):
-
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,default="")
+    content = models.CharField(max_length=200,default="")
+    is_correct = models.BooleanField(default=False) 
+    def __str__(self):
+        return self.choice_text
+ 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
@@ -131,4 +151,8 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice) 
+ 
 #    Other fields and methods you would like to design
